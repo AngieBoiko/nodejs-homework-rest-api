b1bb88896f2164a4964403a4969ebb757680969d
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-07 11:59:12 +0200

    Create addContact.js

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,20 @@
+const { BadRequest } = require('http-errors')
+const { Contact } = require('../../models')
+
+const addContact = async (req, res, next) => {
+    try {
+        const newContact = await Contact.create(req.body)
+        res.json({
+            status: 'success',
+            code: 201,
+            data: {
+                newContact
+            }
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = addContact;
