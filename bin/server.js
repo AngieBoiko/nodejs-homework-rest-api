25e1d874a7931a6d40943b3bea28c1c6c2f93d23
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-07 20:05:16 +0200

    update schema

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,11 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const app = require('../app')
 
-const {PORT=3000,DB_HOST}=process.env
+const { PORT = 3000, DB_HOST } = process.env
 
-
-mongoose.connect(DB_HOST).then(()=>app.listen(PORT)
-).then(()=>console.log( "Database connection successful")).catch((error)=>{
-    console.log(error.message)
-    process.exit(1)})
-
+mongoose.connect(DB_HOST).then(() => app.listen(PORT)
+).then(() => console.log('Database connection successful')).catch((error) => {
+  console.log(error.message)
+  process.exit(1)
+})
