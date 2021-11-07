73ca88814feba676345451e0ff4194bf986cadcd
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-07 10:45:42 +0200

    delete old structure

--- model/index.js
+++
@@ -1,9 +0,0 @@
-const listContacts = require('./listContacts')
-const getContactById = require('./getContactsById')
-const removeContact = require('./removeContact')
-const addContact = require('./addContact')
-const updateContact = require('./updateContact')
-
-const operations = { listContacts, getContactById, removeContact, addContact, updateContact }
-
-module.exports = operations
