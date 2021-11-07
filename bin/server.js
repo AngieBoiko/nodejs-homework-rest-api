1931b1eefcb0d81b597c09764761d889af8dc448
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-07 10:45:04 +0200

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,12 @@
-const mongoose=require('mangoose')
+const mongoose=require('mongoose')
+require('dotenv').config()
 const app = require('../app')
 
 const {PORT=3000,DB_HOST}=process.env
 
 
 mongoose.connect(DB_HOST).then(()=>app.listen(PORT)
-).then(()=>console.log( "Database connection successful")).catch(error)
+).then(()=>console.log( "Database connection successful")).catch((error)=>{
+    console.log(error.message)
+    process.exit(1)})
 
