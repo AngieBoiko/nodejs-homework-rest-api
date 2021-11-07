7da69fa2f83ef4aa2533d9a448e6b405ba897fd3
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-07 12:10:44 +0200

    Update validation.js

--- middlewares/contacts/validation.js
+++ middlewares/contacts/validation.js
@@ -1,14 +1,19 @@
-const{BadRequest}=require('http-errors')
+const { BadRequest } = require('http-errors')
 const { contactJoiSchema } = require('../../models');
 
 
 const validation = (contactJoiSchema) => {
-  const validationMiddleware = (req, _,next) => {
-    const { error } = contactJoiSchema.validate(req.body);
-    if (error) {
-      throw new BadRequest('missing required field')
+  const validationMiddleware = async (req, _, next) => {
+    try {
+      const { error } = await contactJoiSchema.validate(req.body);
+      if (error) {
+        throw new BadRequest('missing required field')
+      }
+      next()
+    } catch (error) {
+      next(error)
     }
-    next()
+  
 
   }
   return validationMiddleware;
