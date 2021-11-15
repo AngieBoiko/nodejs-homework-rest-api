bf1735e9ce5e0a509f433eef6670bc58cd8db212
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-15 21:33:53 +0200

    add logout middleware

--- middlewares/users/authentication.js
+++ middlewares/users/authentication.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken')
 const { Unauthorized } = require('http-errors');
 require('dotenv').config()
+const {User}=require('../../models')
 
 const { SECRET_KEY } = process.env;
 
 const authentication = async (req, _, next) => {
     try {
-        const [type, token] = req.headers.authentication.split(' ');
-        if (type!=='Bearer') {
+        const [type, token] = req.headers.authorization.split(' ');
+        if (type !=='Bearer') {
             throw new Unauthorized('Not authorized')
         }
         try {
             const { id } = jwt.verify(token, SECRET_KEY);
-            const user = user.findById({ id });
+            const user = await User.findById( id );
             if (!user) {
                 throw new Unauthorized('Not authorized')
             }
