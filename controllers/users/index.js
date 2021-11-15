c62c8ea83a147e6dd6f6672c526bddeefb7027dc
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-15 21:35:01 +0200

    add current route

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,4 +1,6 @@
 const signup=require('./signup');
-const login=require('./login')
+const login=require('./login');
+const logout=require('./logout');
+const current=require('./current');
 
-module.exports={signup,login}
+module.exports={signup,login,logout,current};
