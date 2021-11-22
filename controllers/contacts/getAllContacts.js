20e853446915d5013d1a7593b148802b08745d24
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-22 14:58:56 +0200

    add paginate

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,15 +3,26 @@ const { NotFound } = require('http-errors')
 
 const getAllContacts = async (req, res, next) => {
   try {
-    const result = await Contact.find({})
-    if (!result) {
+    const{page=1,limit,favorite}=req.query;  
+    const {_id}=req.user;
+    let pagination=false
+    if(limit){
+      pagination=true
+    }
+    if(favorite){
+      const {docs} = await Contact.paginate({owner:_id, favorite},{page:+page,limit:+limit,pagination})
+    }else{
+    await Contact.paginate({owner:_id},{page:+page,limit:+limit,pagination})
+     return docs
+    }    
+    if (docs.length===0) {
       throw new NotFound()
     }
     res.status(200).json({
       status: 'success',
       code: 200,
       data: {
-        result
+        docs
       }
     })
   } catch (error) {
