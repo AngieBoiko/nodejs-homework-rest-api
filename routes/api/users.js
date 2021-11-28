54232288a971c0c7def02d57d8e091cc4dc90094
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-11-28 16:30:36 +0200

    add avatar for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { userJoiSchema } = require('../../models')
-const { validation, authentication } = require('../../middlewares/users')
+const { validation, authentication,upload } = require('../../middlewares/users')
 const controllers = require('../../controllers/users')
 
 router.post('/signup', validation(userJoiSchema), controllers.signup)
 router.post('/login', validation(userJoiSchema), controllers.login)
 router.get('/logout', authentication, controllers.logout)
 router.get('/current', authentication, controllers.current)
+router.patch('/avatars',authentication,upload.single('avatar'),controllers.changeAvatar)
 
 module.exports = router
